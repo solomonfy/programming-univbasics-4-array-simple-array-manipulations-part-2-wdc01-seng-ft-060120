9948eff7b68b46f949eb514dbc0ff9a5828c0cca
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-22 14:05:13 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_concat(array_1, array_2)
+
+  array_1.concat(array_2)
+end
+
+
+def using_insert(array, new_element)
+  array.insert(4, new_element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+
+def using_flatten(array)
+  array.flatten
+end
+
+
+def using_delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, element)
+  array.delete_at(2)
+end
